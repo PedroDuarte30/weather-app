1e4bcd8eb222e13a71b71538f9957f00f02213f8
Pedro Duarte <info.pedroduarte@gmail.com>
2026-02-22 14:38:01 +0000

    feat: implement fetch logic in api.js with async/await

---
+++ src/js/api.js
@@ -0,0 +1,30 @@
+/**
+ * Function to retrieve meteorological data
+ * @param {string} city - Name of city entered by user
+ * */
+
+async function getWeatherData(city) {
+    const lang = 'pt';
+    const units = 'metric';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}&lang=${lang}`;
+
+    try {
+        const response = await fetch(url);
+
+        //Check if the response was successful (status 200-299)
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error("City not found");
+            }
+            throw new Error("Error loading weather data.");
+        }
+
+        const data = await response.json();
+
+        //Call the function that will handle the display (it will be created in main.js)
+        displayWeather(data);
+    } catch (error) {
+        console.log("API Error:", error.message);
+        showError(error.message);
+    }
+}
